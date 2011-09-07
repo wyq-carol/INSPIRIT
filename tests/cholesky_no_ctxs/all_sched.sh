10a3174cdd1c2fec6def0868e50bcd995f14b6fa
Andra Hugo <andra.hugo@inria.fr>
2011-09-07 12:58:55 +0000

    small modif

--- tests/cholesky_no_ctxs/all_sched.sh
+++ tests/cholesky_no_ctxs/all_sched.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
-export STARPU_DIR=$HOME/sched_ctx/build
+#export STARPU_DIR=$HOME/sched_ctx/build
 
 source sched.sh 
-#source sched.sh 1stchole -chole1
-#source sched.sh 2ndchole -chole2
+source sched.sh 1stchole -chole1
+source sched.sh 2ndchole -chole2
